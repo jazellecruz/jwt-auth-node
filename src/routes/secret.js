98447ade16a07a5550f20ed61ee78408724a39a1
jazzcruz <jazellecruz22@gmail.com>
2023-05-10 17:45:32 +0800

    render secret ejs file

--- src/routes/secret.js
+++ src/routes/secret.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const {isUserAuthenticated} = require("../middlewares/auth");
 
-router.get("/", isUserAuthenticated, (req, res) => {
-  res.send("Welcome to the secret route!!!");
+router.get("/secret", isUserAuthenticated, (req, res) => {
+  res.render("secret");
 });
 
 module.exports = router
