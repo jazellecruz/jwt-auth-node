ae48beef15fbdbd997b4ccc46974f80e8cdc5e03
jazzcruz <jazellecruz22@gmail.com>
2023-05-10 17:47:58 +0800

    add a new model

--- src/models/users.js
+++ src/models/users.js
@@ -2,18 +2,38 @@ const { db } = require("../db/db");
 const bcrypt = require("bcrypt");
 
 const findUserFromDb = async(username, password) => {
-  if (!username || !password) { return undefined };
+  /* incase there is no username and password present in req body 
+  passed onto the function*/ 
+  if (!username || !password) return false;
+  
+  try{
+    let foundUser = await db.promise().query("SELECT * FROM users WHERE username = ?", [username]);
 
-  let foundUser = await db.promise().query("SELECT * FROM users WHERE username = ?", [username]);
+    if(!foundUser[0][0]) return false;
 
-  if(!foundUser[0][0]) { return false };
+    let isUserMatched = await bcrypt.compare(password, foundUser[0][0].password);
 
-  let isUserMatched = await bcrypt.compare(password, foundUser[0][0].password);
-
-  if(!isUserMatched) { return false };
-
-  return foundUser[0][0].username;
+    if(!isUserMatched) return false;
+  
+    return foundUser[0][0].username;
+  } catch(err) {
+    console.log("Error in validating user:", err)
+  }
 
 }
 
-module.exports = { findUserFromDb }
+const findUserFromDbWithUsername = async(username) => {
+
+  try{
+    let foundUser = await db.promise().query("SELECT * FROM users WHERE username = ?", [username]);
+
+    if(!foundUser) return false;
+  
+    return foundUser[0][0].username;
+  } catch(err) {
+    console.log("Error in finding user from database:", err)
+  }
+
+}
+
+module.exports = { findUserFromDb, findUserFromDbWithUsername }
